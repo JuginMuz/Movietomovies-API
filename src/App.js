3432deb38b35e9e1726ba58166e4268b9e299544
Jugin <jugin.muzhaqi7@gmail.com>
2025-09-30 00:04:40 +0100

    Fixing API link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg'
 
 import './App.css'
 
-const API_URL = 'http://www.omdbapi.com?apikey=4b87058';
+const API_URL = 'https://www.omdbapi.com/?apikey=4b87058';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
